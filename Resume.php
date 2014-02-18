e8e49600e1cf14e7a9f027819060905eca167b1a
FloodMike <flood.mike@gmail.com>
2014-02-18 10:34:01 -0600

    Initial commit

---
+++ Resume.php
@@ -0,0 +1,322 @@
+<?php
+    session_start();
+    require 'php_files\functions.php';
+    
+    // This will determine if the script should use functions to fill in data or retrieve data from a database
+    // If there is a database set up, setting this to false would be preferable
+    $useFx = false;
+    
+    $nameFirst = (empty($_GET['fname']) ? "Michael" : $_GET['fname']);
+    $nameLast = (empty($_GET['lname']) ? "Flood" : $_GET['lname']);
+    $jobTitle = (empty($_GET['title']) ? "Software Developer" : $_GET['title']);
+    $mobile = "1 (815) 353-0093";
+    $email = "flood.mike@gmail.com";
+    $linkedIn = "http://www.linkedin.com/in/mikeflood";
+    $gitHub = "https://github.com/FloodMike";
+    
+    if($useFx)
+    {
+        $jobs = get_all_jobs();
+        $schools = get_all_education();
+        $skills = get_all_skills();
+        $interests = array("Running", "Bouldering", "Live music");  
+    }
+    else
+    {
+        $jobs = get_all('jobs');
+        $schools = get_all('education');
+        $skills = get_all('skills');
+        $interests = get_all('interests');
+    }
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> <?php echo $nameFirst . " " . $nameLast . "'s Resume"; ?></title>
+        <link rel="icon" href="images\favicon.png" type="image/x-icon"/>
+        <link rel="stylesheet" type="text/css" href="style\grid.css">
+        <link rel="stylesheet" type="text/css" href="style\stylesheet.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="eight columns">
+                    <div class="row">
+                        <div class="two columns">
+                            &nbsp;
+                        </div>
+                        <div class="ten columns">
+                            <div class="title" id="title"><?php echo $nameFirst . " " . $nameLast; ?></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            &nbsp;
+                        </div>
+                        <div class="ten columns">
+                            <div class="title subtitle"><?php echo $jobTitle; ?></div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        &nbsp;
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            <div class="label">mobile</div>
+                        </div>
+                        <div class="ten columns">
+                            <a href="tel:+18153530093"><?php echo $mobile; ?></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            <div class="label">email</div>
+                        </div>
+                        <div class="ten columns">
+                            <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            <div class="label">web</div>
+                        </div>
+                        <div class="ten columns">
+                            <a href="<?php echo $linkedIn; ?>" target="_blank"><?php echo $linkedIn; ?></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            <div class="label">repository</div>
+                        </div>
+                        <div class="ten columns">
+                            <a href="<?php echo $gitHub; ?>" target="_blank"><?php echo $gitHub; ?></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="two columns">
+                            &nbsp;
+                        </div>
+                        <div class="ten columns" style="margin:15px 0;">
+                            <div id="circle">&nbsp;</div>
+                        </div>
+                    </div>
+                    <?php
+                        if($useFx)
+                        {
+                            foreach($jobs as $job)
+                            {
+                                echo '<div class="row">
+                                            <div class="two columns">
+                                                <div class="label" style="margin-top: 5px;">'. $job->start .'-</div>
+                                            </div>
+                                            <div class="ten columns title"><a href="' . $job->link . '" target="_blank">'. $job->company .'</a></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="two columns">
+                                                <div class="label">'. $job->stop .'</div>
+                                            </div>
+                                            <div class="ten columns skill">'. $job->title .'</div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="two columns">
+                                                &nbsp;
+                                            </div>
+                                            <div class="ten columns">
+                                                <p class="paragraph">'. $job->description .'</p>
+                                            </div>
+                                        </div>';
+                             
+                            }
+                        }
+                        else
+                        {
+                            foreach($jobs as $job)
+                            {
+                                echo '<div class="row">
+                                            <div class="two columns">
+                                                <div class="label" style="margin-top: 5px;">'. $job["Start"] .'-</div>
+                                            </div>
+                                            <div class="ten columns title"><a href="' . $job["Link"] . '" target="_blank">'. $job["Company"] .'</a></div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="two columns">
+                                                <div class="label">'. $job["Stop"] .'</div>
+                                            </div>
+                                            <div class="ten columns skill">'. $job["Title"] .'</div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="two columns">
+                                                &nbsp;
+                                            </div>
+                                            <div class="ten columns">
+                                                <p class="paragraph">'. $job["Description"] .'</p>
+                                            </div>
+                                        </div>';
+                            }
+                        }
+                    ?>    
+                    <div class="row">
+                        <div class="two columns">
+                            &nbsp;
+                        </div>
+                        <div class="ten columns" style="margin:15px 0;">
+                            <div id="circle">&nbsp;</div>
+                        </div>
+                    </div>
+                    <?php  
+                        if($useFx)
+                        {
+                            foreach($schools as $school)
+                            {
+                                echo '<div class="row">
+                                            <div class="two columns">
+                                                <div class="label" style="margin-top: 5px;">'. $school->start .' - ' . $school->stop .'</div>
+                                            </div>
+                                            <div class="ten columns title"><a href="' . $school->link . '" target="_blank">'. $school->name .'</a></div>
+                                        </div>'; 
+                                
+                                if($school->degree)
+                                {
+                                    echo '<div class="row" style="margin-bottom: 10px;">
+                                                <div class="two columns">
+                                                    <div class="label">&nbsp;</div>
+                                                </div>
+                                                <div class="ten columns skill">' . $school->degreeType . ' in ' . $school->major . '</div>
+                                            </div>';
+                                }
+                                elseif(!$school->degree && !is_null($school->major))
+                                {
+                                    echo '<div class="row">
+                                                    <div class="two columns">
+                                                        <div class="label">&nbsp;</div>
+                                                    </div>
+                                                    <div class="ten columns skill">Studied ' . $school->major . '</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="three columns">&nbsp;</div>
+                                                    <div class="nine columns title">&nbsp;</div>
+                                                </div>';
+                                }
+                                else
+                                {
+                                    echo '&nbsp;';
+                                }
+                            }
+                        }
+                        else
+                        {
+                            foreach($schools as $school)
+                            {
+                                echo '<div class="row">
+                                            <div class="two columns">
+                                                <div class="label" style="margin-top: 5px;">'. $school["Start"] .' - ' . $school["Stop"]. '</div>
+                                            </div>
+                                            <div class="ten columns title"><a href="' . $school["Link"] . '" target="_blank">'. $school["Name"] .'</a></div>
+                                        </div>'; 
+                                
+                                if($school["Degree"])
+                                {
+                                    echo '<div class="row" style="margin-bottom: 10px;">
+                                                <div class="two columns">
+                                                    <div class="label">&nbsp;</div>
+                                                </div>
+                                                <div class="ten columns skill">' . $school["Degree_Type"] . ' in ' . $school["Major"] . '</div>
+                                            </div>';
+                                }
+                                elseif(!$school["Degree"] && !is_null($school["Major"]))
+                                {
+                                    echo '<div class="row">
+                                                    <div class="two columns">
+                                                        <div class="label">&nbsp;</div>
+                                                    </div>
+                                                    <div class="ten columns skill">Studied ' . $school["Major"] . '</div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="three columns">&nbsp;</div>
+                                                    <div class="nine columns title">&nbsp;</div>
+                                                </div>';
+                                }
+                                else
+                                {
+                                    echo '&nbsp;';
+                                }
+                            }
+                        }
+                    ?>
+                </div>
+                <div class="four columns">
+                    <div class="row" style="padding: 17px 0;">&nbsp;</div>
+                    <div class="row">
+                        <div class="twelve columns title" style="margin-bottom: 10px;">Computer Skills</div>
+                    </div>
+                    <?php 
+                        if($useFx)
+                        {
+                            foreach($skills as $skill)
+                            {
+                                echo '<div class="row">
+                                            <div class="twelve columns">
+                                                <div class="skill">'.  $skill->name .'</div>
+                                            </div>
+                                        </div>
+                                        <div class="row" style="margin-bottom: 10px;">
+                                            <div class="twelve columns">
+                                                <div class="skill_detail">'.  $skill->experience .'</div>
+                                            </div>
+                                        </div>';
+                            }
+                        }
+                        else
+                        {
+                            foreach($skills as $skill)
+                            {
+                                echo '<div class="row">
+                                            <div class="twelve columns">
+                                                <div class="skill">'.  $skill["Name"] .'</div>
+                                            </div>
+                                        </div>
+                                        <div class="row" style="margin-bottom: 10px;">
+                                            <div class="twelve columns">
+                                                <div class="skill_detail">'.  $skill["Experience"] .'</div>
+                                            </div>
+                                        </div>';
+                            }
+                        }
+                    ?>
+                    <div class="row">
+                        <div class="twelve columns" style="margin:15px 0;">
+                            <div id="circle">&nbsp;</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="twelve columns title" style="margin-bottom: 10px;">Interests</div>
+                    </div>
+                    <?php
+                        if($useFx)
+                        {
+                            foreach($interests as $interest)
+                            {
+                                echo '<div class="row">
+                                            <div class="twelve columns">
+                                                <div class="skill">'.  $interest .'</div>
+                                            </div>
+                                        </div>';
+                            }
+                        }
+                        else
+                        {
+                             foreach($interests as $interest)
+                            {
+                                echo '<div class="row">
+                                            <div class="twelve columns">
+                                                <div class="skill">'.  $interest["Name"] .'</div>
+                                            </div>
+                                        </div>';
+                            }
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ php_files/functions.php
@@ -0,0 +1,134 @@
+<?php
+	require 'classes.php';
+	
+	function get_all($table)
+	{
+		$con = mysqli_connect("localhost", "root","root","resume") or die("Unable to connect to MySQL");
+
+		$sql = 'SELECT * FROM ' . $table;
+		$result = mysqli_query($con, $sql);
+        
+        if($result === FALSE) {
+            die(mysqli_error($con)); // TODO: better error handling
+        }
+        
+		$table = array();
+		while($r = mysqli_fetch_array($result))
+		{
+			$row = array();
+			foreach($r as $k=>$v) {
+				 $row[$k] = $v;
+			}
+			array_push($table,$row);
+			unset($row);
+		}
+		
+		mysqli_close($con);
+		
+		return $table;
+	}
+	
+	function get_all_jobs()
+	{
+		$empower = new Job();
+		$empower->company = "Empower Systems";
+		$empower->title = "Integration/Interface Programmer";
+		$empower->description = "Empower Systems is a software company focused on delivering a hospital EHR solution to clients " .
+											 "across the U.S. Currently I am responsible for scripting and implementing interfacing that " .
+											 "relays HL7 messages from a hospital core system for processing, internally within Empower using " .
+											 "Visual Basic, as well as routing the HL7 message to ancillary systems. Furthermore, I build and deploy various jobs " .
+											 "and services used to process data and update/insert said data into various databases using C# in conjunction with T-SQL. " .
+											 "We used Kanban as our process management system.";
+		$empower->start = "Aug. 2012";
+		$empower->stop = "present";
+		$empower->link = "http://www.empower.md/";
+		
+		$hp = new Job();
+		$hp->company = "Hewlett-Packard";
+		$hp->title = "Intern";
+		$hp->description = "The development team I worked with, within the content management system sector of Hewlett-Packard, " .
+									"required a more efficient way to compile small bits of code within a much larger coding project. To accomplish " .
+									"this, I rewrote their compiler using a combination of JAVA and Perl in just under 6 weeks. The new compiler " .
+									"noticeably increased productivity as well as saved the development team time to be able to work more on the " .
+									"product. The compiler was deployed on a local network web page and was accessible to the entire development " .
+									"team.  We employed SCRUM as our development framework.";
+		$hp->start = "June 2008";
+		$hp->stop = "Aug. 2008";
+		$hp->link = "http://www8.hp.com/us/en/hp-information/index.html";
+		
+		return array($empower, $hp);
+	}
+	
+	function get_all_education()
+	{
+		$roosevelt = new School();
+		$roosevelt->name = "Roosevelt University";
+		$roosevelt->major = "Computer Science";
+		$roosevelt->degree = true;
+		$roosevelt->degreeType = "Bachelor of Science";
+		$roosevelt->start = "2010";
+		$roosevelt->stop = "2012";
+		$roosevelt->link = "http://www.roosevelt.edu/CAS/Programs/CS.aspx";
+		
+		$harper = new School();
+		$harper->name = "William Rainey Harper College";
+		$harper->major = null;
+		$harper->degree = false;
+		$harper->degreeType = null;
+		$harper->start = "2009";
+		$harper->stop = "2010";
+		$harper->link = "http://goforward.harpercollege.edu/";
+		
+		$iowa = new School();
+		$iowa->name = "University of Iowa";
+		$iowa->major = "Software/Biomedical Engineering";
+		$iowa->degree = false;
+		$iowa->degreeType = null;
+		$iowa->start = "2004";
+		$iowa->stop = "2009";
+		$iowa->link = "http://www.engineering.uiowa.edu/";
+		
+		return array($roosevelt, $harper, $iowa);
+	}
+	
+	function get_all_skills()
+	{
+		$c = new Skill();
+		$c->name = "C#/C++";
+		$c->experience = "Utilized best practices with 4 <br />years of experience";
+		
+		$j = new Skill();
+		$j->name = "Java";
+		$j->experience = "3 years of experience";
+		
+		$vb = new Skill();
+		$vb->name = "Visual Basic";
+		$vb->experience="2 years of experience";
+		
+		$hc = new Skill();
+		$hc->name = "Hand coding HTML/CSS";
+		$hc->experience = "Compatible with IE6, IE7 and<br />standards compliant browsers";
+		
+		$we = new Skill();
+		$we->name = "Dreamweaver, Netbeans, Eclipse,<br />Visual Studio";
+		$we->experience = "&nbsp;";
+		
+		$js = new Skill();
+		$js->name = "Javascript/jQuery/AJAX";
+		$js->experience = "Good working knowledge";
+		
+		$sql = new Skill();
+		$sql->name = "SQL";
+		$sql->experience = "Utilized Transact SQL with 2 years<br />of experience as well as MySQL";
+		
+		$prl = new Skill();
+		$prl->name = "Perl";
+		$prl->experience = "Good working knowledge";
+		
+		$php = new Skill();
+		$php->name = "PHP";
+		$php->experience = "Good working knowledge";
+		
+		return array($c,$j,$vb,$hc,$we,$js,$sql,$prl,$php);
+	}
+?>
