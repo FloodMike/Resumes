d62a487350876175ab2c80a019879cae40ddb359
FloodMike <flood.mike@gmail.com>
2014-02-18 11:07:21 -0600

    Update classes.php

--- php_files/classes.php
+++ php_files/classes.php
@@ -1,30 +1,30 @@
 <?php
-
-	class Job
-	{
-		public $company;
-		public $title;
-		public $description;
-		public $start;
-		public $stop;
-		public $link;
-	}
-	
-	class School
-	{
-		public $name;
-		public $major;
-		public $degree;
-		public $degreeType;
-		public $start;
-		public $stop;
-		public $link;
-	}
-	
-	class Skill
-	{
-		public $name;
-		public $experience;
-	}
+    
+    class Job
+    {
+        public $company;
+        public $title;
+        public $description;
+        public $start;
+        public $stop;
+        public $link;
+    }
+    
+    class School
+    {
+        public $name;
+        public $major;
+        public $degree;
+        public $degreeType;
+        public $start;
+        public $stop;
+        public $link;
+    }
+    
+    class Skill
+    {
+        public $name;
+        public $experience;
+    }
 
 ?>
